1efeb95b6f282fb0dc8564ae6ad7eb49a4f631b0
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-04 20:18:36 +0100

    Update

--- sql_scripts/queries/query_5.sql
+++ sql_scripts/queries/query_5.sql
@@ -1,6 +1,6 @@
 -- Find what disciplines the particular teacher teaches.
-SELECT  t.first_name || ' ' || t.last_name AS Teacher,
-        d.discipline_name as Discipline
+SELECT  t.first_name || ' ' || t.last_name AS teacher_name,
+        d.discipline_name as discipline
 FROM    teachers t
 JOIN    disciplines d ON d.teacher_id = t.id
 WHERE   t.id = 5;
