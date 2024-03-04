97bdc0f645686c5e32f08db5b5884a1affd645b7
Aleksander K <alex.nuclearboy@gmail.com>
2024-03-04 21:41:16 +0100

    Fix

--- sql_scripts/queries/query_1.sql
+++ sql_scripts/queries/query_1.sql
@@ -3,6 +3,6 @@ SELECT  s.first_name || ' ' || s.last_name as "Student",
         ROUND(AVG(g.grade), 2) AS "Average grade"
 FROM    students s
 JOIN    grades g ON s.id = g.student_id
-GROUP BY "Student name"
+GROUP BY "Student"
 ORDER BY "Average grade" DESC
 LIMIT 5;
